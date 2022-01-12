80af337520a7a2434553f790407ce6ef51fdaedb
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-01-12 11:49:14 +0100

    Initial Commit

---
+++ FileManager&TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager&TableView
+//
+//  Created by Felipe Ramírez on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FileManager&TableViewTests/FileManager_TableViewTests.swift
@@ -0,0 +1,33 @@
+//
+//  FileManager_TableViewTests.swift
+//  FileManager&TableViewTests
+//
+//  Created by Felipe Ramírez on 12/1/22.
+//
+
+import XCTest
+@testable import FileManager_TableView
+
+class FileManager_TableViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
