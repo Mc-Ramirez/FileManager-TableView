d2a635ceff8661e62f0880ae0e61e62101d70127
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2022-01-12 16:40:41 +0100

    no se lo que hice aqui

---
+++ FileManager&TableView/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  FileManager&TableView
+//
+//  Created by Felipe Ramírez on 12/1/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_name: UILabel!
+    @IBOutlet weak var lbl_lastName: UILabel!
+    @IBOutlet weak var lbl_nameText: UILabel!
+    @IBOutlet weak var lbl_lastNameText: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
